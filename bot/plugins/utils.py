3b0adbdbee179be5831c716bc4a3b7493b3d9e3e
Zoro - Test Account <134167327+zoro-test@users.noreply.github.com>
2023-06-20 09:40:59 +0530

    Update utils.py

--- bot/plugins/utils.py
+++ bot/plugins/utils.py
@@ -75,7 +75,7 @@ def map(pos):
         button = [
             [
              InlineKeyboardButton(text = 'Support Chat', url = SUPPORT_CHAT_LINK),
-             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/viperadnan-git/google-drive-telegram-bot/issues/new")
+             InlineKeyboardButton(text = 'Feature Request', url = "https://t.me/Zoro_StrawHat7")
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
